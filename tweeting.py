6aeb9dce01f2f561e6d38f606a0f05e122caf3c5
QuantumAutoForge <yoyomohitkumarsir@gmail.com>
2025-02-21 12:18:10 +0530

    update

--- tweeting.py
+++ tweeting.py
@@ -32,8 +32,8 @@ def findmean(dateval):
 if __name__ == "__main__":
     print("Processing stored weather data...")
 
-    datelist = [str(datetime.today().date() - relativedelta(days=2)), str(datetime.today().date() - relativedelta(years=1))]
-    datelist=[2025-02-19,2024-02-19]
+    datelist = [str(datetime.today().date() - relativedelta(days=1)), str(datetime.today().date() - relativedelta(years=1))]
+    
     tempdiff = round(findmean(datelist[0]) - findmean(datelist[1]), 2)
     
     print(f'Temperature difference calculated: {tempdiff}')
